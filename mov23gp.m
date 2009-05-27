da94909d230a7b220e0f273a1e64969879e24eee
Yvon <yvon.cognard@gmail.com>
2009-05-27 18:41:16 +0200

    Textmate compliant :)

--- mov23gp.m
+++ mov23gp.m
@@ -1,57 +1,51 @@
-//$ cc mov23gp.m -framework QTKit -framework Foundation
+//$ cc mov23gp.m -framework QTKit -framework Foundation -framework AppKit
 
 #import <QTKit/QTKit.h>
 
 int main (int ac, char**av)
 {
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	QTMovie *movie = [QTMovie movieWithFile:@"/tmp/sample.mov" error:nil];
-	
-	// Handle the first video track 
-	NSArray* trackArray = [movie tracksOfMediaType:QTStringForOSType((OSType)VideoMediaType)];
-	if ( ![trackArray count] ) 
-	{
-		return (-1);
-	}
-	QTTrack* track = nil;
-	track = [trackArray objectAtIndex:0];
-	
-	// Debug informations: dimensions
-	NSLog(@" QTMovieCurrentSizeAttribute %@", NSStringFromSize([[movie attributeForKey:QTMovieCurrentSizeAttribute] sizeValue]));
-	NSLog(@" QTMovieNaturalSizeAttribute %@", NSStringFromSize([[movie attributeForKey:QTMovieNaturalSizeAttribute] sizeValue]));
-	NSLog(@" QTTrackBoundsAttribute      %@", NSStringFromRect([[track attributeForKey:QTTrackBoundsAttribute] rectValue]));
-	NSLog(@" QTTrackDimensionsAttribute  %@", NSStringFromSize([[track attributeForKey:QTTrackDimensionsAttribute] sizeValue]));
-	
-	[movie setAttribute:[NSValue valueWithSize:
-						 NSMakeSize(620, 240)]
-				 forKey: QTMovieCurrentSizeAttribute];
-	
-	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-								[NSNumber numberWithBool:YES], QTMovieExport,
-								[NSNumber numberWithLong:'FLV1'], QTMovieExportType, nil];
-	
-//	[movie writeToFile:@"/tmp/sample.flv" withAttributes:dictionary];
-	
-//	dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-//				  [NSNumber numberWithBool:YES], QTMovieExport,
-//				  [NSNumber numberWithLong:'PICT'], QTMovieExportType,
-//				  @"jpeg", QTAddImageCodecType, nil];
-//	
-//	[movie writeToFile:@"/tmp/sample.jpg" withAttributes:dictionary];
-	
-	QTTime time = QTTimeFromString(@"00:00:00:10.00/1");
-	NSLog(@" Time %@", QTStringFromTime(time));
-	
-	NSImage *image = [movie frameImageAtTime:time];
-	
-	NSLog(@" ImageSize %@", NSStringFromSize([image size]));
-	
-	// Hack to avoid warning. Startup function to call when running Cocoa code from a Carbon application.
-	NSApplicationLoad();
-	[[image TIFFRepresentationUsingCompression:NSJPEGFileType factor:80.0] writeToFile:@"/tmp/sample.jpg" atomically:YES];
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  
+  QTMovie *movie = [QTMovie movieWithFile:@"/tmp/sample.mov" error:nil];
+  
+  // Debug informations: dimensions
+  NSLog(@" QTMovieNaturalSizeAttribute %@", NSStringFromSize([[movie attributeForKey:QTMovieNaturalSizeAttribute] sizeValue]));
+  
+  // ************** //
+  // Encode to FLV  //
+  // ************** //
+  
+  [movie setAttribute:[NSValue valueWithSize:
+             NSMakeSize(620, 240)]
+         forKey: QTMovieCurrentSizeAttribute];
+  
+  NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                [NSNumber numberWithBool:YES], QTMovieExport,
+                [NSNumber numberWithLong:'FLV1'], QTMovieExportType, nil];
+  
+  [movie writeToFile:@"/tmp/sample.flv" withAttributes:dictionary];
+  
+  // ******************** //
+  // Export the thumbnail //
+  // ******************** //
+  
+  [movie setAttribute:[NSValue valueWithSize: NSMakeSize(240, 180)]
+         forKey: QTMovieCurrentSizeAttribute];
+  
+  QTTime time = QTTimeFromString(@"00:00:00:10.00/1");
+  NSLog(@" Time %@", QTStringFromTime(time));
+  
+  NSImage *image = [movie frameImageAtTime:time];
+  
+  NSLog(@" ImageSize %@", NSStringFromSize([image size]));
+  
+  // Hack to avoid warning. Startup function to call when running Cocoa code
+  //  from a Carbon application.
+  NSApplicationLoad();
+  [[image TIFFRepresentationUsingCompression:NSJPEGFileType factor:80.0]
+      writeToFile:@"/tmp/sample.jpg" atomically:YES];
 
-	[pool release];
-	
-	exit(1);
+  [pool release];
+  
+  exit(1);
 }
