8053c0c3db9603d0606e853bf194fef278bf41aa
Yvon <yvon.cognard@gmail.com>
2009-05-28 16:08:52 +0200

    Write files with filename given

--- encoder.m
+++ encoder.m
@@ -23,9 +23,7 @@ QTMovie *open_movie(char *file)
   
   QTMovie *movie = [QTMovie movieWithFile:[NSString stringWithCString:file]
     error:&error];
-  
-  NSLog(@"Error: %x", (int)error);
-  
+    
   if (error != nil)
   {
     fprintf(stderr, "Unable to open video file\n");
@@ -33,13 +31,60 @@ QTMovie *open_movie(char *file)
   }
   
   // Debug informations: dimensions
-  NSLog(@" QTMovieNaturalSizeAttribute %@", 
+  NSLog(@"Dimensions: %@", 
     NSStringFromSize(
       [[movie attributeForKey:QTMovieNaturalSizeAttribute] sizeValue]
     )
   );
   
   return (movie);
+}
+
+NSString *append_path_extension(NSString *path, NSString *ext)
+{
+  NSString *path_with_extension = nil;
+  
+  // Remove the extension (if present), then add the new one.
+  path_with_extension =
+    [[path stringByDeletingPathExtension] stringByAppendingPathExtension:ext];
+          
+  NSLog(@"Path: %@", path_with_extension);
+  
+  return (path_with_extension);
+}
+
+void encode_movie(QTMovie *movie, char *dest)
+{
+  [movie setAttribute:[NSValue valueWithSize:
+             NSMakeSize(320, 240)]
+         forKey: QTMovieCurrentSizeAttribute];
+  
+  NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                [NSNumber numberWithBool:YES], QTMovieExport,
+                [NSNumber numberWithLong:'FLV1'], QTMovieExportType, nil];
+  
+  NSString *dest_with_flv_extension =
+    append_path_extension([NSString stringWithCString:dest], @"flv");
+    
+  [movie writeToFile:dest_with_flv_extension withAttributes:dictionary];
+}
+
+void export_thumbnail(QTMovie *movie, char *dest)
+{
+  [movie setAttribute:[NSValue valueWithSize: NSMakeSize(240, 180)]
+         forKey: QTMovieCurrentSizeAttribute];
+  
+  QTTime time = QTTimeFromString(@"00:00:00:10.00/1");
+  NSLog(@"Thumbnail will be extracted at: %@", QTStringFromTime(time));
+  
+  NSImage *image = [movie frameImageAtTime:time];
+  NSLog(@"Thumbnail size: %@", NSStringFromSize([image size]));
+  
+  NSString *dest_with_jpg_extension =
+    append_path_extension([NSString stringWithCString:dest], @"jpg");
+  
+  [[image TIFFRepresentationUsingCompression:NSJPEGFileType factor:80.0]
+      writeToFile:dest_with_jpg_extension atomically:YES];
 }
 
 int main (int ac, char**av)
@@ -52,50 +97,20 @@ int main (int ac, char**av)
  
   char *input;
   char *output; 
-  if (parse_arguments(ac, av, &input, &output) == -1)
+  
+  // Parse the arguments
+  if (parse_arguments(ac, av, &input, &output) != -1)
   {
-    return (-1);
+    // Open the movie
+    QTMovie *movie = open_movie(input);
+    if ((int)movie != -1)
+    {
+      // Export movie and thumbnail
+      encode_movie(movie, output);
+      export_thumbnail(movie, output);
+    }  
   }  
-  NSLog(@"Input: %s", input);
-  NSLog(@"Output: %s", output);
   
-  QTMovie *movie = open_movie(input);
-  if ((int)movie == -1)
-  {
-    return (-1);
-  }
-  
-  // ************** //
-  // Encode to FLV  //
-  // ************** //
-  
-  [movie setAttribute:[NSValue valueWithSize:
-             NSMakeSize(320, 240)]
-         forKey: QTMovieCurrentSizeAttribute];
-  
-  NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-                [NSNumber numberWithBool:YES], QTMovieExport,
-                [NSNumber numberWithLong:'FLV1'], QTMovieExportType, nil];
-  
-  [movie writeToFile:@"/tmp/sample.flv" withAttributes:dictionary];
-  
-  // ******************** //
-  // Export the thumbnail //
-  // ******************** //
-  
-  [movie setAttribute:[NSValue valueWithSize: NSMakeSize(240, 180)]
-         forKey: QTMovieCurrentSizeAttribute];
-  
-  QTTime time = QTTimeFromString(@"00:00:00:10.00/1");
-  NSLog(@" Time %@", QTStringFromTime(time));
-  
-  NSImage *image = [movie frameImageAtTime:time];
-  
-  NSLog(@" ImageSize %@", NSStringFromSize([image size]));
-  
-
-  [[image TIFFRepresentationUsingCompression:NSJPEGFileType factor:80.0]
-      writeToFile:@"/tmp/sample.jpg" atomically:YES];
 
   [pool release];
   
