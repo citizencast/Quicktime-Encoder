6f9ddb592d81772429ccb80ec7a7e8ad8cbc7da6
Yvon <yvon.cognard@gmail.com>
2009-05-28 15:39:45 +0200

    Parse the arguments

--- encoder.m
+++ encoder.m
@@ -1,22 +1,76 @@
 //$ cc encoder.m -framework QTKit -framework Foundation -framework AppKit
 
 #import <QTKit/QTKit.h>
+#import <stdio.h>
 
-int main (int ac, char**av)
+int parse_arguments(int ac, char **av, char **input, char **output)
 {
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  if (ac != 3)
+  {
+    fprintf(stderr, "Format: %s 'input' 'output'\n", av[0]);
+    return (-1);
+  }
   
-  QTMovie *movie = [QTMovie movieWithFile:@"/tmp/sample.mov" error:nil];
+  *input = av[1];
+  *output = av[2];
+  
+  return (1);
+}
+
+QTMovie *open_movie(char *file)
+{
+  NSError *error = nil;  
+  
+  QTMovie *movie = [QTMovie movieWithFile:[NSString stringWithCString:file]
+    error:&error];
+  
+  NSLog(@"Error: %x", (int)error);
+  
+  if (error != nil)
+  {
+    fprintf(stderr, "Unable to open video file\n");
+    return ((QTMovie *)-1);
+  }
   
   // Debug informations: dimensions
-  NSLog(@" QTMovieNaturalSizeAttribute %@", NSStringFromSize([[movie attributeForKey:QTMovieNaturalSizeAttribute] sizeValue]));
+  NSLog(@" QTMovieNaturalSizeAttribute %@", 
+    NSStringFromSize(
+      [[movie attributeForKey:QTMovieNaturalSizeAttribute] sizeValue]
+    )
+  );
+  
+  return (movie);
+}
+
+int main (int ac, char**av)
+{  
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  
+  // Hack to avoid warning. Startup function to call when running Cocoa code
+  //  from a Carbon application.
+  NSApplicationLoad();
+ 
+  char *input;
+  char *output; 
+  if (parse_arguments(ac, av, &input, &output) == -1)
+  {
+    return (-1);
+  }  
+  NSLog(@"Input: %s", input);
+  NSLog(@"Output: %s", output);
+  
+  QTMovie *movie = open_movie(input);
+  if ((int)movie == -1)
+  {
+    return (-1);
+  }
   
   // ************** //
   // Encode to FLV  //
   // ************** //
   
   [movie setAttribute:[NSValue valueWithSize:
-             NSMakeSize(620, 240)]
+             NSMakeSize(320, 240)]
          forKey: QTMovieCurrentSizeAttribute];
   
   NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
@@ -39,9 +93,7 @@ int main (int ac, char**av)
   
   NSLog(@" ImageSize %@", NSStringFromSize([image size]));
   
-  // Hack to avoid warning. Startup function to call when running Cocoa code
-  //  from a Carbon application.
-  NSApplicationLoad();
+
   [[image TIFFRepresentationUsingCompression:NSJPEGFileType factor:80.0]
       writeToFile:@"/tmp/sample.jpg" atomically:YES];
 
